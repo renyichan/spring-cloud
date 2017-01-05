bfbc9300d0e42447c76604ccd6f818a8ccb093cb
chenrenyi <chenrenyi@jk.cn>
2017-01-05 15:16:43 +0800

    add zuul module

---
+++ spring-cloud-zuul/src/main/java/com/renyi/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package com.renyi.zuul;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by chenrenyi on 2017/1/5.
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+}

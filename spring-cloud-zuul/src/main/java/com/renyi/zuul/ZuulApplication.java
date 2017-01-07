99f3dd748009c804f6cbd2b454dd8762a290069b
chenrenyi <chenrenyi@jk.cn>
2017-01-07 14:18:52 +0800

    clean eureka module,client module,userservice and add change eureka cluster

--- spring-cloud-zuul/src/main/java/com/renyi/zuul/ZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/renyi/zuul/ZuulApplication.java
@@ -3,6 +3,7 @@ package com.renyi.zuul;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by chenrenyi on 2017/1/5.
@@ -10,6 +11,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringCloudApplication
 public class ZuulApplication {
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
     }
